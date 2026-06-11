5f7ef5b45031cbf337426fcb428cc55c5fb0f2b7
Satyam Mandal <satyam.mandal.77@gmail.com>
2026-06-11 19:40:26 +0530

    CLient Code Done , Server Code Pending

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use std::io::{Write, stdout};
+use std::io::{Write, stdout, ErrorKind, Read};
 use crossterm::terminal::{self,Clear,ClearType};
 use crossterm::cursor::{self,MoveTo};
 use crossterm::{QueueableCommand};
@@ -6,6 +6,23 @@ use std::thread;
 use crossterm::{event::poll};
 use crossterm::event::{read,Event,KeyCode,KeyModifiers};
 use std::time::Duration;
+use std::cmp::max;
+use std::str;
+
+use std::net::TcpStream;
+
+struct Rect{ x:u16, y:u16, h:u16, w:u16 }
+
+fn chat_window( stdout: &mut impl Write, chat: &Vec<String>, boundary: Rect) {
+    let n = chat.len();
+    let m = max(0, n  as i32 - boundary.h as i32) as u16;
+
+    for (row,line) in chat.iter().skip(m as usize).enumerate(){
+        stdout.queue(MoveTo(boundary.x, boundary.y + row as u16));
+        let bytes = line.as_bytes();
+        stdout.write(bytes.get(0..boundary.w as usize).unwrap_or(bytes));
+    }
+}
 
 fn funtionality(mut width:u16,mut height:u16){
 
@@ -16,6 +33,9 @@ fn funtionality(mut width:u16,mut height:u16){
     let mut bar = str.repeat(width as usize);
     let mut prompt = String::from("");
 
+    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    let _ =  stream.set_nonblocking(true).unwrap();
+    let mut buf = [0;64];
 
     let mut chat = Vec::new();
 
@@ -32,6 +52,7 @@ fn funtionality(mut width:u16,mut height:u16){
                     match event.code{
                         KeyCode::Char(x) => {
                             if x == 'c' && event.modifiers.contains(KeyModifiers::CONTROL){
+                                terminal::disable_raw_mode().unwrap();
                                 break;
                             }else{
                                 prompt.push(x);
@@ -39,6 +60,7 @@ fn funtionality(mut width:u16,mut height:u16){
                         },
                         KeyCode::Enter =>{
                             chat.push(prompt.clone());
+                            stream.write(&prompt.as_bytes()).unwrap();
                             prompt.clear();
                         },
                         _ => {
@@ -54,19 +76,33 @@ fn funtionality(mut width:u16,mut height:u16){
             }
         }else{
             // no event found
-            stdout.queue(Clear(ClearType::All)).unwrap();
-
-            for (row,message)  in chat.iter().enumerate(){
-                stdout.queue(MoveTo(0,row as u16));
-                stdout.write(message.as_bytes());
+            match stream.read(&mut buf) {
+                Ok(n) => {
+                    chat.push(
+                        std::str::from_utf8(&buf[..n])
+                        .unwrap()
+                        .to_string()
+                    );
+                }
+                Err(err) => {
+                    if err.kind() != std::io::ErrorKind::WouldBlock {
+                        panic!("{err}");
+                    }
+                }
             }
+
+            stdout.queue(Clear(ClearType::All)).unwrap();
+            chat_window(&mut stdout,&chat,Rect{ x:0, y:0, w:width, h:height-2 });
 
             stdout.queue(MoveTo(0,height-2));
             stdout.write(bar.as_bytes());
 
 
             stdout.queue(MoveTo(0,height-1));
-            stdout.write(prompt.as_bytes());
+            {
+                let bytes = prompt.as_bytes();
+                stdout.write(bytes.get(0..width as usize).unwrap_or(bytes));
+            }
 
 
             stdout.flush();
@@ -74,6 +110,7 @@ fn funtionality(mut width:u16,mut height:u16){
         }
     }
 
+    terminal::disable_raw_mode().unwrap();
 
 }
 
@@ -87,5 +124,6 @@ fn main(){
     }else{
         print!("Error");
     }
+    stdout.flush().unwrap();
     println!("client");
 }
